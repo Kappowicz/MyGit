5ef5ca9456368522048cded556dd6c200b88a531
jacob <113104296+Kappowicz@users.noreply.github.com>
2025-10-04 14:33:14 +0200

    continue work on git commit, clean some code, start work on debugging functions

--- src/mygit.cpp
+++ src/mygit.cpp
@@ -10,6 +10,9 @@ const std::string INDEX_FILE_NAME = "/index";
 const std::string EXEC_CHAR = "e";
 const std::string NOT_EXEC_CHAR = "n";
 
+const std::string INDEX_FILE_LOCALIZATION = MAIN_FOLDER_NAME + INDEX_FILE_NAME;
+const std::string OBJECTS_FOLDER_LOCALIZATION = MAIN_FOLDER_NAME + OBJECTS_FOLDER_NAME;
+
 void printHelp() {
   std::cout << "this is my simple implementation of git \n start by initializing the repo with ./mygit init " <<
       std::endl;
@@ -25,10 +28,10 @@ void initMyGit() {
       std::cout << "folder " << MAIN_FOLDER_NAME << " failed to create" << std::endl;
   }
 
-  if (std::filesystem::create_directory(MAIN_FOLDER_NAME + OBJECTS_FOLDER_NAME)) {
-    std::cout << "folder " << MAIN_FOLDER_NAME + OBJECTS_FOLDER_NAME << " created" << std::endl;
+  if (std::filesystem::create_directory(OBJECTS_FOLDER_LOCALIZATION)) {
+    std::cout << "folder " << OBJECTS_FOLDER_LOCALIZATION << " created" << std::endl;
   } else
-    std::cout << "folder " << MAIN_FOLDER_NAME + OBJECTS_FOLDER_NAME << " failed to create" << std::endl;
+    std::cout << "folder " << OBJECTS_FOLDER_LOCALIZATION << " failed to create" << std::endl;
 }
 
 std::string calculateHash(const std::string &fileName) //something like djb2
@@ -51,7 +54,7 @@ std::string calculateHash(const std::string &fileName) //something like djb2
 
 void MyGitAdd(const std::string &fileName) {
   std::string hash = calculateHash(fileName);
-  std::string fileDestination = MAIN_FOLDER_NAME + OBJECTS_FOLDER_NAME + "/" + hash;
+  std::string fileDestination = OBJECTS_FOLDER_LOCALIZATION + "/" + hash;
   if (std::filesystem::exists(fileDestination)) {
     std::cout << "File " << fileDestination << " already exists" << std::endl;
     return;
@@ -62,7 +65,7 @@ void MyGitAdd(const std::string &fileName) {
 }
 
 void addToIndex(const std::string &fileName, std::string hash) {
-  std::ofstream file(MAIN_FOLDER_NAME + INDEX_FILE_NAME);
+  std::ofstream file(INDEX_FILE_LOCALIZATION, std::ios::app);
   if (!file.is_open()) {
     throw std::runtime_error("Failed to open the file: " + fileName);
   }
@@ -71,10 +74,44 @@ void addToIndex(const std::string &fileName, std::string hash) {
   std::filesystem::perms permisions = status.permissions();
   std::string currExecChar = permisions == std::filesystem::perms::owner_exec ? EXEC_CHAR : NOT_EXEC_CHAR;
 
-  std::string output = currExecChar + " " + hash + " " + fileName;
+  std::string output = currExecChar + " " + hash + " " + fileName + "\n";
 
   file.write(output.c_str(), output.size());
 }
 
 void MyGitCommit() {
+  /* sample commit object
+tree f314a9254316e1a92a54466b81bdd09415c44136   <- this commit tree hash from index file
+parent                                          <- previous commit hash from index file
+Initial commit with siema.txt and ok.png        <- this commit's message
+   */
+  //Create tree object
+  std::ifstream file(INDEX_FILE_LOCALIZATION, std::ios::binary);
+  if (!file.is_open()) {
+    throw std::runtime_error("Failed to open the file: " + INDEX_FILE_LOCALIZATION);
+  }
+  std::string word;
+  //for now there are only 3 words in each line of index file, exec rights, hash and localization, to update later
+  int amountOfWordsInLine = 3;
+  int currentWordCount = 3;
+  while (file >> word) {
+    // displaying content
+
+    if (currentWordCount % amountOfWordsInLine == 0) {
+      //Exec rights word
+      std::cout << " Exec rights: " << word;
+      currentWordCount = 0;
+    } else if (currentWordCount % amountOfWordsInLine == 1) {
+      std::cout << " Hash: " << word;
+    } else if (currentWordCount % amountOfWordsInLine == 2) {
+      std::cout << " File path: " << word;
+      std::cout << std::endl;
+    } else {
+      std::cout << " Something different: " << word;
+    }
+
+    //std::cout << " " << word;
+
+    currentWordCount++;
+  }
 }
