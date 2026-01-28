bb0509984eeb2230a976e640c1c050914b803f60
jacob <113104296+Kappowicz@users.noreply.github.com>
2026-01-28 13:55:57 +0100

    temporary predefined clang directory

--- src/build_and_test.sh
+++ src/build_and_test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ustaw zmienne środowiskowe raz jeszcze dla pewności
+export CXX=/opt/homebrew/opt/llvm/bin/clang++
+export CC=/opt/homebrew/opt/llvm/bin/clang
+export LDFLAGS="-L/opt/homebrew/opt/llvm/lib/c++ -Wl,-rpath,/opt/homebrew/opt/llvm/lib/c++"
+
+
 cd ..
 mkdir -p build
 cd build || exit #in case cd fails
