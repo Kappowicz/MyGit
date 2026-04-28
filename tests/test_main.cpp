4ba20c57e5a58e244d8e8891e42fc94f24fe87b6
jacob <113104296+Kappowicz@users.noreply.github.com>
2026-04-28 12:26:35 +0200

    Add simple tests with gtest

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,10 +1,18 @@
 #include <gtest/gtest.h>
+#include "../include/mygit.h"
+#include <filesystem>
+namespace fs = std::filesystem;
 
-TEST(MyGitBasicTest, CheckAssertions) {
+TEST(MyGitBasicTest, TemplateAssertion) {
   EXPECT_TRUE(true);
   EXPECT_EQ(2 + 2, 4);
 }
 
-TEST(MyGitBasicTest, CheckAssertions2) {
-  EXPECT_EQ(2 + 2, 4);
+TEST(MyGitBasicTest, TestMyGitInit) {
+  MyGitInit();
+  EXPECT_TRUE(fs::exists(".mygit"));
+  EXPECT_TRUE(fs::is_directory(".mygit"));
+
+  fs::remove_all(".mygit");
 }
+
